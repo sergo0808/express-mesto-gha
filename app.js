6d68af1466e2dd4dcb28e462022297b8a4255a8c
Savitskiy <sovick90@bk.ru>
2022-09-05 11:27:24 +0300

    fix 002

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const { createUser, login, getUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 
+const NOT_FOUND_CODE = 404;
+const SERVER_ERROR_CODE = 500;
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
@@ -24,6 +27,20 @@ app.use(auth);
 app.use(userRouter);
 app.use(cardRouter);
 app.get('/users/me', getUser);
+
+app.use('*', (_, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: 'Страница не найдена' });
+});
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else {
+    res.status(SERVER_ERROR_CODE).send({ message: err.message });
+  }
+
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const router = require('express').Router();
 const {
   getUsers,
+  getUser,
   updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
+const {
+  userValidate,
+  userIdValidate,
+  avatarValidate,
+} = require('../middlewares/validation');
+
 router.get('/users', getUsers);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+router.get('/:userId', userIdValidate, getUser);
+router.patch('/users/me', userValidate, updateUser);
+router.patch('/users/me/avatar', avatarValidate, updateAvatar);
 
 module.exports = router;
